55742c8d020b1d8390c157a9e5f19ca9338439b5
Filip Grace <filip@filbot.com>
2026-01-22 11:27:25 -0800

    refactor: Move hardware interface initialization to `module_init` and improve resource cleanup in `module_exit`.

--- lib/waveshare_epd/epdconfig.py
+++ lib/waveshare_epd/epdconfig.py
@@ -49,117 +49,99 @@ class RaspberryPi:
     SCLK_PIN = 11
 
     def __init__(self):
-        import spidev
-        import gpiozero
-        
-        self.SPI = spidev.SpiDev()
-        self.GPIO_RST_PIN    = gpiozero.LED(self.RST_PIN)
-        self.GPIO_DC_PIN     = gpiozero.LED(self.DC_PIN)
-        # self.GPIO_CS_PIN     = gpiozero.LED(self.CS_PIN)
-        self.GPIO_PWR_PIN    = gpiozero.LED(self.PWR_PIN)
-        self.GPIO_BUSY_PIN   = gpiozero.Button(self.BUSY_PIN, pull_up = False)
-
-        
+        self.SPI = None
+        self.GPIO_RST_PIN = None
+        self.GPIO_DC_PIN = None
+        self.GPIO_PWR_PIN = None
+        self.GPIO_BUSY_PIN = None
 
     def digital_write(self, pin, value):
         if pin == self.RST_PIN:
-            if value:
-                self.GPIO_RST_PIN.on()
-            else:
-                self.GPIO_RST_PIN.off()
+            if self.GPIO_RST_PIN:
+                if value:
+                    self.GPIO_RST_PIN.on()
+                else:
+                    self.GPIO_RST_PIN.off()
         elif pin == self.DC_PIN:
-            if value:
-                self.GPIO_DC_PIN.on()
-            else:
-                self.GPIO_DC_PIN.off()
-        # elif pin == self.CS_PIN:
-        #     if value:
-        #         self.GPIO_CS_PIN.on()
-        #     else:
-        #         self.GPIO_CS_PIN.off()
+            if self.GPIO_DC_PIN:
+                if value:
+                    self.GPIO_DC_PIN.on()
+                else:
+                    self.GPIO_DC_PIN.off()
         elif pin == self.PWR_PIN:
-            if value:
-                self.GPIO_PWR_PIN.on()
-            else:
-                self.GPIO_PWR_PIN.off()
+            if self.GPIO_PWR_PIN:
+                if value:
+                    self.GPIO_PWR_PIN.on()
+                else:
+                    self.GPIO_PWR_PIN.off()
 
     def digital_read(self, pin):
-        if pin == self.BUSY_PIN:
+        if pin == self.BUSY_PIN and self.GPIO_BUSY_PIN:
             return self.GPIO_BUSY_PIN.value
-        elif pin == self.RST_PIN:
-            return self.RST_PIN.value
-        elif pin == self.DC_PIN:
-            return self.DC_PIN.value
-        # elif pin == self.CS_PIN:
-        #     return self.CS_PIN.value
-        elif pin == self.PWR_PIN:
-            return self.PWR_PIN.value
+        elif pin == self.RST_PIN and self.GPIO_RST_PIN:
+            return self.GPIO_RST_PIN.value
+        elif pin == self.DC_PIN and self.GPIO_DC_PIN:
+            return self.GPIO_DC_PIN.value
+        elif pin == self.PWR_PIN and self.GPIO_PWR_PIN:
+            return self.GPIO_PWR_PIN.value
+        return 0
 
     def delay_ms(self, delaytime):
         time.sleep(delaytime / 1000.0)
 
     def spi_writebyte(self, data):
-        self.SPI.writebytes(data)
+        if self.SPI:
+            self.SPI.writebytes(data)
 
     def spi_writebyte2(self, data):
-        self.SPI.writebytes2(data)
-
-    def DEV_SPI_write(self, data):
-        self.DEV_SPI.DEV_SPI_SendData(data)
-
-    def DEV_SPI_nwrite(self, data):
-        self.DEV_SPI.DEV_SPI_SendnData(data)
-
-    def DEV_SPI_read(self):
-        return self.DEV_SPI.DEV_SPI_ReadData()
+        if self.SPI:
+            self.SPI.writebytes2(data)
 
     def module_init(self, cleanup=False):
-        self.GPIO_PWR_PIN.on()
-        
-        if cleanup:
-            find_dirs = [
-                os.path.dirname(os.path.realpath(__file__)),
-                '/usr/local/lib',
-                '/usr/lib',
-            ]
-            self.DEV_SPI = None
-            for find_dir in find_dirs:
-                val = int(os.popen('getconf LONG_BIT').read())
-                logging.debug("System is %d bit"%val)
-                if val == 64:
-                    so_filename = os.path.join(find_dir, 'DEV_Config_64.so')
-                else:
-                    so_filename = os.path.join(find_dir, 'DEV_Config_32.so')
-                if os.path.exists(so_filename):
-                    self.DEV_SPI = CDLL(so_filename)
-                    break
-            if self.DEV_SPI is None:
-                RuntimeError('Cannot find DEV_Config.so')
+        import spidev
+        import gpiozero
 
-            self.DEV_SPI.DEV_Module_Init()
-
-        else:
-            # SPI device, bus = 0, device = 0
+        if self.SPI is None:
+            self.SPI = spidev.SpiDev()
             self.SPI.open(0, 0)
             self.SPI.max_speed_hz = 4000000
             self.SPI.mode = 0b00
+            
+        if self.GPIO_RST_PIN is None:
+            self.GPIO_RST_PIN = gpiozero.LED(self.RST_PIN)
+        if self.GPIO_DC_PIN is None:
+            self.GPIO_DC_PIN = gpiozero.LED(self.DC_PIN)
+        if self.GPIO_PWR_PIN is None:
+            self.GPIO_PWR_PIN = gpiozero.LED(self.PWR_PIN)
+        if self.GPIO_BUSY_PIN is None:
+            self.GPIO_BUSY_PIN = gpiozero.Button(self.BUSY_PIN, pull_up=False)
+
+        self.GPIO_PWR_PIN.on()
         return 0
 
     def module_exit(self, cleanup=False):
         logger.debug("spi end")
-        self.SPI.close()
+        if self.SPI:
+            self.SPI.close()
+            self.SPI = None
 
-        self.GPIO_RST_PIN.off()
-        self.GPIO_DC_PIN.off()
-        self.GPIO_PWR_PIN.off()
         logger.debug("close 5V, Module enters 0 power consumption ...")
-        
-        if cleanup:
+        if self.GPIO_PWR_PIN:
+            self.GPIO_PWR_PIN.off()
+
+        # Always close GPIOs to release pins
+        if self.GPIO_RST_PIN:
             self.GPIO_RST_PIN.close()
+            self.GPIO_RST_PIN = None
+        if self.GPIO_DC_PIN:
             self.GPIO_DC_PIN.close()
-            # self.GPIO_CS_PIN.close()
+            self.GPIO_DC_PIN = None
+        if self.GPIO_PWR_PIN:
             self.GPIO_PWR_PIN.close()
+            self.GPIO_PWR_PIN = None
+        if self.GPIO_BUSY_PIN:
             self.GPIO_BUSY_PIN.close()
+            self.GPIO_BUSY_PIN = None
 
         
 
